cbf2e4ca82058c63bd6d591210af7b2df1bc217d
yapingxin <xinyp@live.com>
2017-01-23 22:02:55 +0800

    Update source code indentation to 4 <space>.

--- Dev/src/lib/C-Thread-Pool/test/test1.c
+++ Dev/src/lib/C-Thread-Pool/test/test1.c
@@ -18,42 +18,44 @@ static void task2();
 
 int run_test1()
 {
-	
-	puts("Making threadpool with 12 threads");
-	threadpool thpool = thpool_init(12);
-
-	puts("Adding 40 tasks to threadpool");
-	int i;
-	for (i=0; i<20; i++){
-		thpool_add_work(thpool, (void*)task1, NULL);
-		thpool_add_work(thpool, (void*)task2, NULL);
-	};
-
-	puts("Killing threadpool");
-	
-	thpool_wait(thpool);
-	thpool_destroy(thpool);
-	
-	return 0;
+    int i;
+    
+    puts("Making threadpool with 12 threads");
+    threadpool thpool = thpool_init(12);
+    
+    puts("Adding 40 tasks to threadpool");
+    
+    for (i=0; i<20; i++)
+    {
+        thpool_add_work(thpool, (void*)task1, NULL);
+        thpool_add_work(thpool, (void*)task2, NULL);
+    };
+    
+    puts("Killing threadpool");
+    
+    thpool_wait(thpool);
+    thpool_destroy(thpool);
+    
+    return 0;
 }
 
 static void task1()
 {
-	sleep(7);
-	
-	pthread_mutex_lock(&mutex1);
-	task1_count++;
-	printf("Thread #%u working on *** task1: %d\n", (int)pthread_self(), task1_count);
-	pthread_mutex_unlock(&mutex1);
+    sleep(7);
+    
+    pthread_mutex_lock(&mutex1);
+    task1_count++;
+    printf("Thread #%u working on *** task1: %d\n", (int)pthread_self(), task1_count);
+    pthread_mutex_unlock(&mutex1);
 }
 
 
 static void task2()
 {
-	sleep(2);
-	
-	pthread_mutex_lock(&mutex2);
-	task2_count++;
-	printf("Thread #%u working on --- task2: %d\n", (int)pthread_self(), task2_count);
-	pthread_mutex_unlock(&mutex2);
+    sleep(2);
+    
+    pthread_mutex_lock(&mutex2);
+    task2_count++;
+    printf("Thread #%u working on --- task2: %d\n", (int)pthread_self(), task2_count);
+    pthread_mutex_unlock(&mutex2);
 }
